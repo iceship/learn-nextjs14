45c91f368ce785e6749ebcd476b22ed9cc3cf506
Iceship <iceship@gmail.com>
2024-03-24 11:12:04 +0900

    first commit

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Not Found',
+};
+
+export default function NotFound() {
+  return <div>
+    <h1>404 - Page Not Found</h1>
+  </div>
+}
---
+++ components/movie-info.tsx
@@ -0,0 +1,30 @@
+import { API_URL } from '../app/constants'
+import styles from '../styles/movie-info.module.css'
+
+export async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`)
+  return response.json()
+}
+
+export default async function MovieInfo({ id }: { id: string }) {
+  const movie = await getMovie(id)
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.poster}
+        src={movie.poster_path}
+        alt={movie.title}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐{movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        {movie.homepage && (
+          <a href={movie.homepage} target="_blank" rel="noreferrer">
+            Homepage &rarr;
+          </a>
+        )}
+      </div>
+    </div>
+  )
+}
